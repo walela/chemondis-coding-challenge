d032a2700d42175e958c67fd84b6629f486a9e77
Austin Walela <austinmasibo@gmail.com>
2022-12-08 13:15:09 +0300

    make navbar sticky

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,7 +4,7 @@ import logo from '@/assets/gallery_512.png'
 function Layout() {
   return (
     <div className='w-full'>
-      <div className=' bg-violet-700 w-full mx-auto py-4 mb-6 shadow'>
+      <div className=' bg-violet-700 w-full mx-auto py-4 mb-6 shadow-md sticky top-0 z-50'>
         <div className='w-96 mx-auto flex gap-4 items-center'>
           <img src={logo} width={48} alt='logo' />
           <h1 className='font-logo text-4xl text-center text-white font-bold tracking-wider border-b-2 border-white'>
